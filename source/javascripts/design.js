cd5fde7af1e0cf3bce6a94f36abded70524a3f6e
Adelon Zeta <hello@adelonzeta.com>
2015-02-17 19:50:47 +0800

    design updates: profile editing ui, zero notification view, empty feed view.

--- source/javascripts/design.js
+++ source/javascripts/design.js
@@ -31,4 +31,37 @@ $(document).ready(function () {
 	$(window).resize(function(){
 	    $('.photoset-grid-basic').photosetGrid();
 	});
+	
+	$(".edit-profile").click(function () {
+		var calculated_height = $(document).height();
+		$(".profile-editing-overlay").show().height(calculated_height + 70);
+		$(".header-photo").addClass("editing");
+		$(".profile-photo").css({
+			"position": "relative",
+			"z-index": "999999"
+		});
+		$(".saved-view").hide();
+		$(".editing-view").show();
+		$(".editing-name").focus();
+		$(".editing-cover-overlay").show();
+		$(".editing-cover-button").show();
+		$(".profile header").css("padding", "180px 0 0");
+		$(".editing-photo").show();
+		$(".navbar").css("z-index","9999999");
+		$(".nav-overlay").show();
+	});
+	
+	$(".cancel-editing").click(function () {
+		$(".profile-editing-overlay").hide();
+		$(".header-photo").removeClass("editing");
+		$(".profile-photo").removeAttr("style");
+		$(".saved-view").show();
+		$(".editing-view").hide();
+		$(".editing-cover-overlay").hide();
+		$(".editing-cover-button").hide();
+		$(".profile header").removeAttr("style");
+		$(".editing-photo").hide();
+		$(".navbar").removeAttr("style");
+		$(".nav-overlay").hide();
+	});
 });
